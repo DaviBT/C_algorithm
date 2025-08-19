44c3ac2711efd7f9284b26abf37f5a6e9a3dc8d3
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2025-08-19 23:45:40 +0000

    aula 2

---
+++ exs/conversor_unidades/conversor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int escolha;
+
+    do{
+        // menu
+        printf("------MENU------");
+        printf("Digite o número para fazer sua escolha: \n");
+        printf("1. Celsius para Fahrenheit");
+        printf("2. metros para km");
+        printf("1. segundos para minutos");
+
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            celsiusToF();
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+
+}
