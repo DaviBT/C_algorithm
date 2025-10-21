7ef86750f039d4e9965021a6fdcfefe4080ef867
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2025-10-21 23:35:57 +0000

    numeros inteiros e suas somas no arquivo

--- exs/arquivos/01/num_inteiros.c
+++ exs/arquivos/01/num_inteiros.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 
 int main(){
-    int reset();
 
     int five = 5;
     int numbers[5];
     int num = 0;
 
-    for(int i = 0; i < 5; i++){
-        FILE *f = fopen("numbers.txt", "a");
-        if (f == NULL){
+    FILE *file = fopen("numbers.txt", "w");
+        if (file == NULL){
             printf("Erro ao abrir o arquivo");
             return 1;
         }
+
+    for(int i = 0; i < 5; i++){
+        
         printf("Digite um numero: \n");
         scanf("%d", &num);
 
-        fprintf(f, "%d", num);
+        fprintf(file, "%d\n", num);
 
-        fclose(f);
+        
     }
+    fclose(file);
+    
+    printf("numeros no arquivo: \n");
+    file = fopen("numbers.txt", "r");
+    while (!feof(file)) {
+        if (fscanf(file, "%d", &num) == 1) {
+            printf("%d\n", num);
+        }
+    }
+    fclose(file);
+
+    int soma = 0;
+    file = fopen("numbers.txt", "r");
+    while(!feof(file)){
+        if(fscanf(file, "%d", &num) == 1){
+            soma += num;
+        }
+    }
+    printf("o resultado da soma de todos os numeros e: %d \n", soma);
 
     return 0;
 }
 
-int reset(){
-    FILE *f = fopen("numbers.txt", "w");
-    fprintf(f, "%s", "");
-    fclose(f);
-
-    return 0;
-}
 
